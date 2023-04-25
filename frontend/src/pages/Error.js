0e8a5495d4b1d1342b8556e77aaf41b61cdf03a2
Rodrigo Reis <rreisp@gmail.com>
2023-04-25 07:49:01 -0300

    The json() Utility Function

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,16 +1,16 @@
 import { useRouteError } from "react-router-dom";
 
-import PageContent from "./PageContent";
+import PageContent from "../components/PageContent";
 import MainNavigation from "../components/MainNavigation";
 
 function ErrorPage() {
   const error = useRouteError();
 
   let title = "An error occurred!";
-  let message = "Some went wrong!";
+  let message = "Something went wrong!";
 
   if (error.status === 500) {
-    message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
 
   if (error.status === 404) {
